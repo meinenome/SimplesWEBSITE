977a49cd7bb23fda80a4244843044bf9e5badecc
Danniel Junior <danmeunome@gmail.com>
2025-02-02 14:19:51 +1100

    OK

---
+++ script.js
@@ -0,0 +1,7 @@
+// JavaScript file for future enhancements
+document
+  .getElementById('contact-form')
+  .addEventListener('submit', function (event) {
+    event.preventDefault();
+    alert('Message sent! We will get back to you soon.');
+  });
